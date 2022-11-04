f204de61e8a3893d654a3a86285f4dff5988a493
HwanGonJang <myggona@gmail.com>
2022-11-04 13:47:26 +0900

    feat: jwt security logic

--- src/main/kotlin/com/example/yourssuassignment/SecurityConfig.kt
+++ src/main/kotlin/com/example/yourssuassignment/SecurityConfig.kt
@@ -1,18 +1,59 @@
 package com.example.yourssuassignment
 
+import com.example.yourssuassignment.application.jwt.JwtAccessDeniedHandler
+import com.example.yourssuassignment.application.jwt.JwtAuthenticationEntryPoint
+import com.example.yourssuassignment.application.jwt.JwtFilter
+import com.example.yourssuassignment.application.jwt.TokenProvider
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig: WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http
+class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+) {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @get: Bean
+    val jwtFilter: JwtFilter
+        get() = JwtFilter(tokenProvider)
+
+    @get: Bean
+    val accessDeniedHandler: AccessDeniedHandler
+        get() = JwtAccessDeniedHandler()
+
+    @get: Bean
+    val authenticationEntryPoint: AuthenticationEntryPoint
+        get() = JwtAuthenticationEntryPoint()
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
             .cors().and().csrf().disable()
             .exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPoint)
+            .accessDeniedHandler(accessDeniedHandler)
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/auth/**", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(
+                jwtFilter,
+                UsernamePasswordAuthenticationFilter::class.java,
+            )
+            .build()
     }
 }
---
+++ src/main/kotlin/com/example/yourssuassignment/application/jwt/JwtFilter.kt
@@ -0,0 +1,33 @@
+package com.example.yourssuassignment.application.jwt
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtFilter(
+    private val tokenProvider: TokenProvider,
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val token = resolveToken(request)
+
+        if (StringUtils.hasText(token)) {
+            SecurityContextHolder.getContext().authentication = tokenProvider.validateToken(token)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader("Authorization")
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer "))
+            return bearerToken.substring(7)
+        return null
+    }
+}
